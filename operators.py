e0d643886367f48eb09c28b4b9514dcb43feef00
shreevardhan <shreemandlik40@gmail.com>
2024-07-22 03:02:05 -0700

    Assignament Operators

---
+++ operators.py
@@ -0,0 +1,15 @@
+print("Assignment Operation")
+x=3
+print("Value of X is ",(x))
+x+=5
+print("Value of X is ",(x))
+x-=5
+print("Value of X is ",(x))
+x*=5
+print("Value of X is ",(x))
+x/=1
+print("Value of X is ",(x))
+x%=5
+print("Value of X is ",(x))
+
+
